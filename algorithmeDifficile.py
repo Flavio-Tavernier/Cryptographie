744db88b243a5f7e2b315fd7b9c0c0cae3d40e3a
Flavio <flavio.tavernier2@gmail.com>
2024-03-06 11:48:04 +0100

    OK chiffrement affine --> manque dechiffrement

--- algorithmeDifficile.py
+++ algorithmeDifficile.py
@@ -1,45 +1,39 @@
-listeChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'à', 'é', 'è', 'ê', 'ë', 'ù', 'ï', 'î', 'À', 'É', 'È', 'Ê', 'Ë', 'Ù', 'Ï', 'Î', '.', '?', '!', ':', '.', ',', '/', '\\', '$', '§', '£', '%', '=', '+', '-', '*', '\'', '\"', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
-listeCharsInverses = ['0', '9', '8', '7', '6', '5', '4', '3', '2', '1', '\"', '\'', '*', '-', '+', '=', '%', '£', '§', '$', '\\', '/', ',', '.', ':', '!', '?', '.', 'Î', 'Ï', 'Ù', 'Ë', 'Ê', 'È', 'É', 'À', 'î', 'ï', 'ù', 'ë', 'ê', 'è', 'é', 'à', 'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A', 'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
+listeChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 
-print(listeChars[0+1])
+def cryptText(listeChars, lettre, a, b) :
+    clearSentence = "abcd"
+    cryptedSentence = ""
 
 
-def chiffrerPhrase() :
-    phrase = input("Entrez la phrase que vous souhaitez chiffrer : ")
+    for lettre in clearSentence :
+        indexOfLetter = getIndexOfLetter(listeChars, lettre)
+        indexAfterAffine = applyAffineFunction(indexOfLetter, a, b)
+        indexAfterAffineAndModulo = applyModulo(indexAfterAffine)
+        cryptedLetter = getCryptedLetter(listeChars, indexAfterAffineAndModulo)
 
-    phraseChiffre = inverserChars(phrase, listeChars, listeCharsInverses)
+        cryptedSentence += cryptedLetter
 
-    return phraseChiffre
-
-
-
-def inverserChars(phrase, listeChars, listeCharsInverses) :
-    phraseChiffre = ""
-
-    for lettre in phrase :
-        if lettre == " " :
-            phraseChiffre += " "
-        else :
-            #Recupere num lettre inverse
-            numLettreSuivante = listeChars.index(lettre) + 1
-
-            #Recupere la lettre inverse dans la liste alphabet normal par rapport au num lettre inverse
-            lettreSuivante = listeChars[numLettreSuivante]
-
-            #Concatene la chaine de caracteres
-            phraseChiffre +=  lettreSuivante
-
-
-    print("\n\t\tPhrase déchiffrée : " + phrase)
-    print("\t\tPhrase chiffrée : " + phraseChiffre)
+    print("Non codé : " + clearSentence)
+    print("Codé : " + cryptedSentence)
 
 
 
+def getIndexOfLetter(listeChars, lettre) :
+    return listeChars.index(lettre)
 
 
+def applyAffineFunction(indexOfLetter, a, b) :
+    return a * indexOfLetter + b
+
+
+def applyModulo(indexAfterAffine) :
+    return indexAfterAffine % 26
+
+
+def getCryptedLetter(listeChars, indexAfterAffineAndModulo) :
+    return listeChars[indexAfterAffineAndModulo]
 
 
 
