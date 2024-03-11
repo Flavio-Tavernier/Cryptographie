f55c9f86d6e3446cd3fd1caa8968fdd06afdd989
Flavio <flavio.tavernier2@gmail.com>
2024-03-11 11:03:15 +0100

    KO essai nouvelle interface sans succès

--- programme.py
+++ programme.py
@@ -6,6 +6,207 @@ from tkinter import font
 from tkinter import Tk, Label
 
 
+
+
+
+
+listeChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'à', 'é', 'è', 'ê', 'ë', 'ù', 'ï', 'î', 'À', 'É', 'È', 'Ê', 'Ë', 'Ù', 'Ï', 'Î', '.', '?', '!', ':', '.', ',', '/', '\\', '$', '§', '£', '%', '=', '+', '-', '*', '\'', '\"', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+listeCharsInverses = ['0', '9', '8', '7', '6', '5', '4', '3', '2', '1', '\"', '\'', '*', '-', '+', '=', '%', '£', '§', '$', '\\', '/', ',', '.', ':', '!', '?', '.', 'Î', 'Ï', 'Ù', 'Ë', 'Ê', 'È', 'É', 'À', 'î', 'ï', 'ù', 'ë', 'ê', 'è', 'é', 'à', 'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A', 'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
+
+
+
+nbChars = len(listeChars)
+
+
+def chiffrerPhrase() :
+    phrase = entryClearSentencePage2.get()
+
+    print(phrase)
+
+    phraseChiffre = inverserChars(phrase, listeChars, listeCharsInverses)
+
+    print(phraseChiffre)
+
+    displayCryptedSentencePage2.delete(0, "end")
+    displayCryptedSentencePage2.insert(0, phraseChiffre)
+
+
+def dechiffrerPhrase() :
+    phraseChiffre = entryCryptedSentencePage2.get()
+
+    phraseDechiffre = retablirChars(phraseChiffre)
+
+    displayClearSentencePage2.delete(0, "end")
+    displayClearSentencePage2.insert(0, phraseDechiffre)
+
+
+
+def inverserChars(phrase, listeChars, listeCharsInverses) :
+    phraseChiffre = ""
+
+    for lettre in phrase :
+        if lettre == " " :
+            phraseChiffre += " "
+        else :
+            #Recupere num lettre inverse
+            numLettreInverse = listeCharsInverses.index(lettre)
+
+            #Recupere la lettre inverse dans la liste alphabet normal par rapport au num lettre inverse
+            lettreInverse = listeChars[numLettreInverse]
+
+            #Concatene la chaine de caracteres
+            phraseChiffre +=  lettreInverse
+
+
+    print("\n\t\tPhrase déchiffrée : " + phrase)
+    print("\t\tPhrase chiffrée : " + phraseChiffre)
+    return phraseChiffre
+
+
+
+
+def retablirChars(phraseChiffre) :
+    phraseDechiffre = ""
+
+
+    for lettre in phraseChiffre :
+        if lettre == " " :
+            phraseDechiffre += " "
+        else :
+            #Recupere num lettre inverse
+            numLettreALendroit = listeChars.index(lettre)
+
+            #Recupere la lettre inverse dans la liste alphabet normal par rapport au num lettre inverse
+            lettreNormale = listeCharsInverses[numLettreALendroit]
+
+            #Concatene la chaine de caracteres
+            phraseDechiffre +=  lettreNormale
+
+    print("\n\t\tPhrase chiffrée : " + phraseChiffre)
+    print("\t\tPhrase déchiffrée : " + phraseDechiffre)
+    return phraseDechiffre
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def crypt() :
+    clefA = int(entryClefA.get())
+    clefB = int(entryClefB.get())
+    clearSentence = entryClearSentencePage3.get()
+
+    cryptedSentence = ""
+
+
+    for lettre in clearSentence :
+        if lettre == " " :
+            cryptedSentence += " "
+        else :
+            indexOfLetter = getIndexOfLetter(listeChars, lettre)
+            indexAfterAffine = applyAffineFunction(indexOfLetter, clefA, clefB)
+            indexAfterAffineAndModulo = applyModulo(indexAfterAffine)
+            cryptedLetter = getCryptedLetter(listeChars, indexAfterAffineAndModulo)
+
+            cryptedSentence += cryptedLetter
+
+    displayCryptedSentencePage3.delete(0, 'end')
+    displayCryptedSentencePage3.insert(0, cryptedSentence)
+    return cryptedSentence
+
+
+
+
+def getIndexOfLetter(listeChars, lettre) :
+    return listeChars.index(lettre)
+
+
+def applyAffineFunction(indexOfLetter, clefA, clefB) :
+    return clefA * indexOfLetter + clefB
+
+
+def applyModulo(indexAfterAffine) :
+    return indexAfterAffine % nbChars
+
+
+def getCryptedLetter(listeChars, indexAfterAffineAndModulo) :
+    return listeChars[indexAfterAffineAndModulo]
+
+
+
+
+
+# Dechiffrement
+def decrypt():
+    clefA = int(entryClefAToDecrypt.get())
+    clefB = int(entryClefBToDecrypt.get())
+    cryptedSentence = entryCryptedSentencePage3.get()
+    decryptedSentence = ""
+
+    for lettre in cryptedSentence :
+        if lettre == " " :
+            decryptedSentence += " "
+        else :
+            decryptedSentence += affineDecrypting(clefA,clefB,lettre, listeChars)
+
+    displayDecryptedSentencePage3.delete(0, 'end')
+    displayDecryptedSentencePage3.insert(0, decryptedSentence)
+    return decryptedSentence
+
+
+def affineDecrypting(clefA,clefB,lettre,listeChars):
+
+    indexOfCryptedLetter = getIndexOfLetter(listeChars, lettre)
+
+    indexOfClearLetter =(inverse(clefA)*(indexOfCryptedLetter-clefB)) % nbChars
+
+    return listeChars[indexOfClearLetter]
+
+
+def inverse(clefA):
+        indexOfClearLetter=0
+        while (clefA * indexOfClearLetter % nbChars != 1):
+                indexOfClearLetter = indexOfClearLetter + 1
+        return indexOfClearLetter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 window = tk.Tk()
 f = font.Font(family='Bahnschrift')
 style1 = font.Font(size=16)
@@ -212,176 +413,6 @@ window.mainloop()
 
 
 
-
-listeChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'à', 'é', 'è', 'ê', 'ë', 'ù', 'ï', 'î', 'À', 'É', 'È', 'Ê', 'Ë', 'Ù', 'Ï', 'Î', '.', '?', '!', ':', '.', ',', '/', '\\', '$', '§', '£', '%', '=', '+', '-', '*', '\'', '\"', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
-listeCharsInverses = ['0', '9', '8', '7', '6', '5', '4', '3', '2', '1', '\"', '\'', '*', '-', '+', '=', '%', '£', '§', '$', '\\', '/', ',', '.', ':', '!', '?', '.', 'Î', 'Ï', 'Ù', 'Ë', 'Ê', 'È', 'É', 'À', 'î', 'ï', 'ù', 'ë', 'ê', 'è', 'é', 'à', 'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A', 'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
-
-
-
-nbChars = len(listeChars)
-
-
-def chiffrerPhrase() :
-    phrase = entryClearSentencePage2.get()
-
-    print(phrase)
-
-    phraseChiffre = inverserChars(phrase, listeChars, listeCharsInverses)
-
-    print(phraseChiffre)
-
-    displayCryptedSentencePage2.delete(0, "end")
-    displayCryptedSentencePage2.insert(0, phraseChiffre)
-
-
-def dechiffrerPhrase() :
-    phraseChiffre = entryCryptedSentencePage2.get()
-
-    phraseDechiffre = retablirChars(phraseChiffre)
-
-    displayClearSentencePage2.delete(0, "end")
-    displayClearSentencePage2.insert(0, phraseDechiffre)
-
-
-
-def inverserChars(phrase, listeChars, listeCharsInverses) :
-    phraseChiffre = ""
-
-    for lettre in phrase :
-        if lettre == " " :
-            phraseChiffre += " "
-        else :
-            #Recupere num lettre inverse
-            numLettreInverse = listeCharsInverses.index(lettre)
-
-            #Recupere la lettre inverse dans la liste alphabet normal par rapport au num lettre inverse
-            lettreInverse = listeChars[numLettreInverse]
-
-            #Concatene la chaine de caracteres
-            phraseChiffre +=  lettreInverse
-
-
-    print("\n\t\tPhrase déchiffrée : " + phrase)
-    print("\t\tPhrase chiffrée : " + phraseChiffre)
-    return phraseChiffre
-
-
-
-
-def retablirChars(phraseChiffre) :
-    phraseDechiffre = ""
-
-
-    for lettre in phraseChiffre :
-        if lettre == " " :
-            phraseDechiffre += " "
-        else :
-            #Recupere num lettre inverse
-            numLettreALendroit = listeChars.index(lettre)
-
-            #Recupere la lettre inverse dans la liste alphabet normal par rapport au num lettre inverse
-            lettreNormale = listeCharsInverses[numLettreALendroit]
-
-            #Concatene la chaine de caracteres
-            phraseDechiffre +=  lettreNormale
-
-    print("\n\t\tPhrase chiffrée : " + phraseChiffre)
-    print("\t\tPhrase déchiffrée : " + phraseDechiffre)
-    return phraseDechiffre
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def crypt() :
-    clefA = int(entryClefA.get())
-    clefB = int(entryClefB.get())
-    clearSentence = entryClearSentencePage3.get()
-
-    cryptedSentence = ""
-
-
-    for lettre in clearSentence :
-        if lettre == " " :
-            cryptedSentence += " "
-        else :
-            indexOfLetter = getIndexOfLetter(listeChars, lettre)
-            indexAfterAffine = applyAffineFunction(indexOfLetter, clefA, clefB)
-            indexAfterAffineAndModulo = applyModulo(indexAfterAffine)
-            cryptedLetter = getCryptedLetter(listeChars, indexAfterAffineAndModulo)
-
-            cryptedSentence += cryptedLetter
-
-    displayCryptedSentencePage3.delete(0, 'end')
-    displayCryptedSentencePage3.insert(0, cryptedSentence)
-    return cryptedSentence
-
-
-
-
-def getIndexOfLetter(listeChars, lettre) :
-    return listeChars.index(lettre)
-
-
-def applyAffineFunction(indexOfLetter, clefA, clefB) :
-    return clefA * indexOfLetter + clefB
-
-
-def applyModulo(indexAfterAffine) :
-    return indexAfterAffine % nbChars
-
-
-def getCryptedLetter(listeChars, indexAfterAffineAndModulo) :
-    return listeChars[indexAfterAffineAndModulo]
-
-
-
-
-
-# Dechiffrement
-def decrypt():
-    clefA = int(entryClefAToDecrypt.get())
-    clefB = int(entryClefBToDecrypt.get())
-    cryptedSentence = entryCryptedSentencePage3.get()
-    decryptedSentence = ""
-
-    for lettre in cryptedSentence :
-        if lettre == " " :
-            decryptedSentence += " "
-        else :
-            decryptedSentence += affineDecrypting(clefA,clefB,lettre, listeChars)
-
-    displayDecryptedSentencePage3.delete(0, 'end')
-    displayDecryptedSentencePage3.insert(0, decryptedSentence)
-    return decryptedSentence
-
-
-def affineDecrypting(clefA,clefB,lettre,listeChars):
-
-    indexOfCryptedLetter = getIndexOfLetter(listeChars, lettre)
-
-    indexOfClearLetter =(inverse(clefA)*(indexOfCryptedLetter-clefB)) % nbChars
-
-    return listeChars[indexOfClearLetter]
-
-
-def inverse(clefA):
-        indexOfClearLetter=0
-        while (clefA * indexOfClearLetter % nbChars != 1):
-                indexOfClearLetter = indexOfClearLetter + 1
-        return indexOfClearLetter
 
 
 
